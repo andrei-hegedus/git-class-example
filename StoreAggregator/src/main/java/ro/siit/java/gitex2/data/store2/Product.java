efdead046d35735eba4dc9ede55a1c5c55b2dc3d
DESKTOP-Q5896S2\Tavi <doicaoctavian2k@gmail.com>
2017-10-30 20:39:04 +0200

    Implemented Store2

--- StoreAggregator/src/main/java/ro/siit/java/gitex2/data/store2/Product.java
+++ StoreAggregator/src/main/java/ro/siit/java/gitex2/data/store2/Product.java
@@ -4,4 +4,35 @@ package ro.siit.java.gitex2.data.store2;
  * Created by andrei on 10/30/17.
  */
 public class Product {
+    private String name;
+    private String type;
+    private int stock;
+
+    public Product(String name, String type, int stock){
+        this.name = name;
+        this.type = type;
+        this.stock = stock;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", stock=" + stock +
+                '}';
+    }
 }
--- StoreAggregator/src/main/java/ro/siit/java/gitex2/data/store2/Store2.java
+++ StoreAggregator/src/main/java/ro/siit/java/gitex2/data/store2/Store2.java
@@ -4,6 +4,19 @@ package ro.siit.java.gitex2.data.store2;
  * Created by andrei on 10/30/17.
  */
 public class Store2 {
-    private Product [] products;
-    private int [] prices;
+    private Product [] products = new Product[3];
+    private int [] prices = {500, 350, 150};
+
+    public void populateProducts(){
+        products[0] = new Product("iphone x", "smartphone", 3);
+        products[1] = new Product("galaxy s8", "smartphone", 10);
+        products[2] = new Product("huawei P10", "smartphone", 1);
+
+        System.out.println(products[1]);
+    }
+
+    public static void main(String[] args) {
+        Store2 s2 = new Store2();
+        s2.populateProducts();
+    }
 }
