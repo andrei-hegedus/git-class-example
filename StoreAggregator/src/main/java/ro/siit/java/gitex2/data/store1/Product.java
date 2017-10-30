28299539a4c4d784711109301f65c48dc5146188
CatalinaPapiu <catalina172001@yahoo.com>
2017-10-30 20:41:15 +0200

    Commited changes to Store1.

--- StoreAggregator/src/main/java/ro/siit/java/gitex2/data/store1/Product.java
+++ StoreAggregator/src/main/java/ro/siit/java/gitex2/data/store1/Product.java
@@ -4,4 +4,22 @@ package ro.siit.java.gitex2.data.store1;
  * Created by andrei on 10/30/17.
  */
 public class Product {
+    String name;
+    float price;
+    String type;
+   public Product(String name, float price, String type) {
+       this.name=name;
+       this.price=price;
+       this.type=type;
+   }
+
+   public String getName() {
+       return name;
+   }
+    public float getPrice() {
+        return price;
+    }
+    public String getType() {
+        return type;
+    }
 }
--- StoreAggregator/src/main/java/ro/siit/java/gitex2/data/store1/Stock.java
+++ StoreAggregator/src/main/java/ro/siit/java/gitex2/data/store1/Stock.java
@@ -4,4 +4,19 @@ package ro.siit.java.gitex2.data.store1;
  * Created by andrei on 10/30/17.
  */
 public class Stock {
+    String name;
+    int nr;
+
+    public Stock(String name, int nr) {
+        this.name = name;
+        this.nr = nr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNr() {
+        return nr;
+    }
 }
